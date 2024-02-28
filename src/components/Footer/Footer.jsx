e81730b4f1ccf3f851d1da7681883a2bfaaec788
code0em <146350167+Code0Em@users.noreply.github.com>
2024-02-28 20:21:42 +0000

    added home and footer components, content still to be added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+//=============
+//  Functions
+//=============
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>Still to be made...</p>
+            </footer>
+        </>
+    )
+};
+
+//===========
+//  Exports
+//===========
+export default Footer;
