36257c639cefe702a75b06f60185a149311c6931
YasmiinLDN <yasmin.a.ibrahim@icloud.com>
2024-02-28 22:15:09 +0000

    followed the figma design to create a basic navbar or menu on the top

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+
+//=============
+//  Functions
+//=============
+
+const Home = () => {
+    return (
+        <>
+            <div className="ui top text fixed menu">
+                <div className="item">
+                    <img src="./images/paw-solid.svg"></img>
+                </div>
+                <h1>Bark vs. Meow: The Battle for the Couch</h1>
+                <div className="item">
+                    <img src="./images/cat-solid.svg"></img>
+                </div>
+                {/* <a class="item">Cat API</a>
+                <a class="item">Dog API</a>
+                <a class="item">Contact Us</a> */}
+            </div>
+        </>
+    )
+};
+
+//===========
+//  Exports 
+//===========
+export default Home;
