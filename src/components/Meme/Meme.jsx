b60632bbddb508a6b0ca8608437c34fd7408f398
code0em <146350167+Code0Em@users.noreply.github.com>
2024-03-01 20:18:53 +0000

    added styling to meme text and btn function to change meme text

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,6 +1,7 @@
 // imports
 import React, { useState, useEffect } from 'react';
 import { catApiUrl, dogApiUrl } from '../utils/constraints.js';
+import Button from 'react-bootstrap/Button';
 
 // store API key
 const apiKey = 'live_js1I9MDcTXDFqs4EQ1NCaHf1c5rasF2iT24oFqHepOKCFKtPXgcHgPDtIV0BG4dz';
@@ -10,6 +11,10 @@ const Meme = ({ animalType }) => {
     const [memeText, setMemeText] = useState({ topLine: "", bottomLine: "" });
     const [image, setImage] = useState('');
     const [loading, setLoading] = useState(true);
+    // States for meme text colour change (inc the button)
+    const [textColor, setTextColor] = useState('black')
+    const [btnTheme, setBtnTheme] = useState('light')
+    const [btnText, setBtnText] = useState('Light')
 
     // useEffect hook fetches the image URL when the animalType prop changes (to cat or dog)
     useEffect(() => {
@@ -34,7 +39,7 @@ const Meme = ({ animalType }) => {
             .catch(error => console.error(`Error fetching ${animalType} images:`, error));
     }, [animalType]);
 
-    // update memeText upon user input
+    // updates memeText upon user input
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setMemeText(prev => ({
@@ -42,6 +47,13 @@ const Meme = ({ animalType }) => {
             [name]: value,
         }));
     };
+
+    // Change meme text color on click
+    const handleColorChange = () => {
+        setTextColor(textColor === 'black' ? 'white' : 'black');
+        setBtnTheme(btnTheme === 'dark' ? 'light' : 'dark');
+        setBtnText(btnText === 'Dark' ? 'Light' : 'Dark');
+    }
 
     // return structured JSX - image currently restricted to 300px width (this can be changed)
     return (
@@ -62,13 +74,20 @@ const Meme = ({ animalType }) => {
                     onChange={handleInputChange}
                     placeholder="Enter bottom line of meme" />
             </form>
+
+            {/* ES NOTE: This can be taken out if we decide we don't want this functionality: atm it changes meme text colour */}
+            <div>
+            <Button variant={btnTheme} onClick={handleColorChange}>{btnText} Text</Button>{' '}
+            </div>
+
             {loading ? (
                 <p>Loading...</p>
             ) : (
                 <section className="meme-container">
                     <img src={image} className="meme-img" alt={`${animalType} Meme`} style={{ maxWidth: '300px' }} />
-                    <p className="meme-top-line">{memeText.topLine}</p>
-                    <p className="meme-bottom-line">{memeText.bottomLine}</p>
+                    {/* Includes dynamic text color */}
+                    <p className="meme-top-line" style={{ color: textColor }}>{memeText.topLine}</p>
+                    <p className="meme-bottom-line" style={{ color: textColor }}>{memeText.bottomLine}</p>
                 </section>
             )}
         </>
