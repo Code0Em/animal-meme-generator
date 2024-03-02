c42f422bf88a38dafac76cdb0a5eb6b060ed3470
code0em <146350167+Code0Em@users.noreply.github.com>
2024-03-02 12:09:34 +0000

    set up testing environment and configured RTL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// Includes test property
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+  }
 })
